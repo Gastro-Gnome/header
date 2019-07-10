50233424b7b5506149c8e19cff3ec2d2565bc6c2
BrandonSYu <beyu94@gmail.com>
2019-07-09 18:14:10 -0700

    Fix to setupTest and .gitignore, Added several components

--- client/src/setupTest.js
+++ client/src/setupTest.js
@@ -1,5 +1,5 @@
 //import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, {shallow, render, mount} from 'enzyme';
+import Enzyme from 'enzyme';
 
 Enzyme.configure({ adapter : new Adapter()});
