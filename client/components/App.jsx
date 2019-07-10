883786fbef537c3db67d26754e07d8a8721a3084
BrandonSYu <beyu94@gmail.com>
2019-07-09 17:27:59 -0700

    Revert "Fix"
    This reverts commit 47b6c249a9ddca6c4aad7b52ef05b9b3dc0a4b40.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,8 +8,6 @@ const App = () => (
 <div>
 <svg>
         <circle cx={50} cy={50} r={10} fill="red" />
-        <line x1="36.36363636363637" y1="0" x2="36.36363636363637" y2="182" style="stroke:gray;fill:none;stroke-width:0.2" class="grid">
-</line>
       </svg>
 </div>
 );
