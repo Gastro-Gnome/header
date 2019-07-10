170e0e0d0009923ce0f7c4fab9428b9ff0841f6c
BrandonSYu <beyu94@gmail.com>
2019-07-09 17:06:31 -0700

    Fix setupTests and added to .gitignore. Also added several components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import DetailView from './DetailView.jsx';
+import Navbar from './Navbar.jsx';
+import LineChart from './LineChart.jsx';
+
 
 const App = () => (
-  <div>
-    Hello World!
-  </div>
+<div>
+<svg>
+        <circle cx={50} cy={50} r={10} fill="red" />
+      </svg>
+</div>
 );
 
 export default App;
--- client/src/setupTest.js
+++ client/src/setupTest.js
@@ -1,5 +1,4 @@
-//import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, {shallow, render, mount} from 'enzyme';
+import Enzyme, {shallow} from 'enzyme';
 
 Enzyme.configure({ adapter : new Adapter()});
