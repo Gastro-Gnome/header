56d607d50505b34b137d10c498e1e5f591f7d3f2
BrandonSYu <beyu94@gmail.com>
2019-07-09 17:28:49 -0700

    Revert "Fix setupTests and added to .gitignore. Also added several components"
    This reverts commit 170e0e0d0009923ce0f7c4fab9428b9ff0841f6c.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import DetailView from './DetailView.jsx';
-import Navbar from './Navbar.jsx';
-import LineChart from './LineChart.jsx';
-
 
 const App = () => (
-<div>
-<svg>
-        <circle cx={50} cy={50} r={10} fill="red" />
-      </svg>
-</div>
+  <div>
+    Hello World!
+  </div>
 );
 
 export default App;
--- client/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar = () => {
-    <div> 
-        Navbar here 
-    </div>
-}
-
-export default Navbar;
